a084de62cdb312e1c1d151566135a69b8903249e
Maycon Fabio <m.vinnicyus@gmail.com>
2021-10-31 22:50:29 -0300

    feat: Add tables via JS

--- script.js
+++ script.js
@@ -54,29 +54,66 @@ function TP1(p1, p2, trab, atv) {
   mencoes('Técnicas de Programação 1', notas)
 }
 
-const cn_p1 = 5.6
-const cn_p2 = 7.2
-const cn_p3 = null
-const cn_atv = 0
-CN(cn_p1, cn_p2, cn_p3, cn_atv)
+function constructDisciplina(title, data) {
+  const disciplina = document.createElement('div')
+  disciplina.classList.add('disciplina')
 
-const pe_p1 = 7
-const pe_p2 = 10
-const pe_p3 = 10
-PE(pe_p1, pe_p2, pe_p3)
+  const titleElement = document.createElement('h2')
+  titleElement.innerText = title
+  disciplina.appendChild(titleElement)
 
-const a1_p1 = 5.8
-const a1_p2 = null
-const a1_nq = 9.2
-const a1_nt = 6.831
-ALG1(a1_p1, a1_p2, a1_nq, a1_nt)
-// 9.9*.3*.4 + 6.831*.7*.4 + 5.8*.4*.6 + x*.6*.6
+  const table = document.createElement('table')
+  table.classList.add('table')
+  disciplina.appendChild(table)
+  const trs = Object.keys(data).map(key => {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `<td>${key}</td><td>${data[key]}</td>`
+    return tr
+  })
 
-const tp1_p1 = 10
-const tp1_p2 = null
-const tp1_trab = 9.5
-const tp1_atv = 10
-TP1(tp1_p1, tp1_p2, tp1_trab, tp1_atv)
+  trs.forEach(tr => table.appendChild(tr))
 
+  document.body.appendChild(disciplina)
+}
 
+function render() {
+  const cnData = {
+    'Prova 1': 5.6,
+    'Prova 2': 7.2,
+    'Prova 3': null,
+    'Atividades': 0,
+  }
+  CN(cnData['Prova 1'], cnData['Prova 2'], cnData['Prova 3'], cnData['Atividades'])
+  
+  const peData = {
+    prova1: 7,
+    prova2: 10,
+    prova3: 10,
+  }
+  PE(peData.prova1, peData.prova2, peData.prova3)
+  
+  const a1Data = {
+    prova1: 5.8,
+    prova2: null,
+    nq: 9.2,
+    nt: 6.831,
+  }
+  ALG1(a1Data.prova1, a1Data.prova2, a1Data.nq, a1Data.nt)
+  // 9.9*.3*.4 + 6.831*.7*.4 + 5.8*.4*.6 + x*.6*.6
+  
+  const tp1Data = {
+    prova1: 10,
+    prova2: null,
+    trab: 9.5,
+    atv: 10,
+  }
+  TP1(tp1Data.prova1, tp1Data.prova2, tp1Data.trab, tp1Data.atv)
+
+  constructDisciplina('Cálculo numérico', cnData)
+  constructDisciplina('Probabilidade e Estatística', peData)
+  constructDisciplina('Álgebra 1', a1Data)
+  constructDisciplina('Técnicas de Programação 1', tp1Data)
+}
+
+render()
 
